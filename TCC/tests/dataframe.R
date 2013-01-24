a2f62a60d45d2951657a5bd8db07db712ca521ed
Tomoaki NISHIYAMA <tomoakin@kenroku.kanazawa-u.ac.jp>
2013-01-24 21:28:45 +0900

    Use non automatic row.names in the data.frame.

--- TCC/tests/dataframe.R
+++ TCC/tests/dataframe.R
@@ -1,6 +1,7 @@
 library(TCC)
 data(hypoData)
-df<-data.frame(names = hypoData[,0],
+df<-data.frame(row.names = paste('a', rownames(hypoData), sep=""),
   A1 = hypoData[,1], A2 = hypoData[,2], A3 = hypoData[,3],
   B1 = hypoData[,4], B2 = hypoData[,5], B3 = hypoData[,6])
 tccdata=new("TCC",df,c(3,3))
+head(tccdata$names)
