fb39520e8bf6c053099a7fcfe7204d2202c10aec
kohijiri <kohijiri@gmail.com>
2013-03-27 15:56:40 +0900

    add GLM (multi-group, multi-factor) test scripts.

---
+++ TCC/tests/normByDeseqGLM.DEGES3.R
@@ -0,0 +1,13 @@
+if (Sys.getenv("TCC_REAL_TEST") != ""){
+  library(TCC)
+  group <- c(1, 1, 1, 2, 2, 2, 3, 3, 3)
+  fit1 <- count ~ condition
+  fit0 <- count ~ 1
+  tcc <- generateSimulationData(Ngene = 100, PDEG = 0.2, DEG.assign = c(0.6, 0.2, 0.2),
+                                group = group)
+  tcc <- calcNormFactors(tcc, norm.method = "deseq", test.method = "deseq", 
+                         fit1 = fit1, fit0 = fit0, iteration = 3)
+  cat("tcc$norm.factors: ")
+  cat(tcc$norm.factors)
+  cat("\n")
+}
---
+++ TCC/tests/normByEdgerGLM.DEGES3.R
@@ -0,0 +1,19 @@
+if (Sys.getenv("TCC_REAL_TEST") != ""){
+  library(TCC)
+  group <- c(1, 1, 1, 2, 2, 2, 3, 3, 3)
+  design <- model.matrix(~ factor(group))
+  tcc <- generateSimulationData(Ngene = 100, PDEG = 0.2, DEG.assign = c(0.6, 0.2, 0.2),
+                                group = group)
+  tcc <- calcNormFactors(tcc, norm.method = "tmm", test.method = "edger", 
+                         design = design, coef = 2:3, iteration = 3)
+  cat("tcc$norm.factors: ")
+  cat(tcc$norm.factors)
+  cat("\n")
+
+  design <- model.matrix(~ 0 + factor(group))
+  tcc <- calcNormFactors(tcc, norm.method = "tmm", test.method = "edger", 
+                         design = design, contrast = c(-1, 0, 1), iteration = 3)
+  cat("tcc$norm.factors: ")
+  cat(tcc$norm.factors)
+  cat("\n")
+}
