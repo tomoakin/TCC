9306d5abd11a44383decd725c2fc0b60ad882b4a
Tomoaki NISHIYAMA <tomoakin@kenroku.kanazawa-u.ac.jp>
2013-04-06 17:45:08 +0900

    Perform the test unconditionally. set.seed to get the same result every time.

--- TCC/tests/DESDES.R
+++ TCC/tests/DESDES.R
@@ -1,12 +1,11 @@
-if (Sys.getenv("TCC_REAL_TEST")!=""){
-  library(TCC)
-  tcc <- generateSimulationData(Ngene = 100, PDEG = 0.2, DEG.assign = c(0.9, 0.1))
-  tcc <- calcNormFactors(tcc, norm.method = "deseq", test.method = "deseq",
-                          FDR = 0.1, floorPDEG = 0.05)
-  cat("tcc$norm.factors: ")
-  cat(tcc$norm.factors)
-  cat("\n")
-  cat("tcc$stat$execution.time: ")
-  cat(tcc$stat$execution.time)
-  cat("\n")
-}
+library(TCC)
+set.seed(1314)
+tcc <- generateSimulationData(Ngene = 100, PDEG = 0.2, DEG.assign = c(0.9, 0.1))
+tcc <- calcNormFactors(tcc, norm.method = "deseq", test.method = "deseq",
+                        FDR = 0.1, floorPDEG = 0.05)
+cat("tcc$norm.factors: ")
+cat(tcc$norm.factors)
+cat("\n")
+cat("tcc$stat$execution.time: ")
+cat(tcc$stat$execution.time)
+cat("\n")
