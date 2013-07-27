b7f08e341bdd7d980c5c034507f7a0fc19f543f4
kohijiri <kohijiri@gmail.com>
2013-07-27 16:01:46 +0900

    remove

--- TCC/inst/unitTests/test_calcAUCValue.R
+++
@@ -1,9 +0,0 @@
-test_calcAUCValue <- function() {
-    tcc <- simulateReadCounts()
-    tcc <- calcNormFactors(tcc)
-    tcc <- estimateDE(tcc)
-    auc <- calcAUCValue(tcc)
-
-    checkTrue(is.numeric(auc))
-}
-
